d7c954ad54c920d38e732970830fb2a971546100
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-04-17 09:54:11 +0100

    feat :added quiz logique

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.quizapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,19 +16,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etEmail,etPassword;
+    Button bLogin;
+    TextView tvRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+       // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
 
-    public void navigateToRegister(View view) {
-        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+        etEmail=findViewById(R.id.email);
+        etPassword=findViewById(R.id.password);
+        bLogin=findViewById(R.id.bLogin);
+        tvRegister=findViewById(R.id.textViewRegister);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(etEmail.getText().toString().equals("abde") &&
+                etPassword.getText().toString().equals("2025")
+                ){
+                    Intent i1=new Intent(MainActivity.this, Quiz1.class);
+                    startActivity(i1);
+                }else{
+                    Toast.makeText(getApplicationContext(),"Login or password incorrect",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/quizapp/Quiz5.java
+++ app/src/main/java/com/example/quizapp/Quiz5.java
@@ -1,6 +1,12 @@
 package com.example.quizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Quiz5 extends AppCompatActivity {
 
+    Button bNext;
+    RadioGroup rg;
+    RadioButton rb;
+    int score;
+    String correctResp="radioButton3";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_quiz5);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bNext=findViewById(R.id.bNext);
+        rg=findViewById(R.id.rg);
+
+        bNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(rg.getCheckedRadioButtonId()==-1){
+                    Toast.makeText(getApplicationContext(),"Veuillez choisir une reponse svp",Toast.LENGTH_SHORT).show();
+                }else{
+                    rb=findViewById(rg.getCheckedRadioButtonId());
+                    score=getIntent().getIntExtra("score",0);
+                    if(rb.getText().toString().equals(correctResp)){
+                        score+=1;
+                    }
+                    Intent i1=new Intent(Quiz5.this, Score.class);
+                    i1.putExtra("score",score);
+                    startActivity(i1);
+                }
+            }
         });
     }
 }
